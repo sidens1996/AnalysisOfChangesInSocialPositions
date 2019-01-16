44a16cccfbc6e58f30c7afdb95482cf037849932
sidens1996 <1424285844@qq.com>
2019-01-16 09:51:16 +0800

    修复job数据访问错误

---
+++ display/src/main/java/com/hust/display/controller/AdminPageController.java
@@ -0,0 +1,54 @@
+package com.hust.display.controller;
+
+import com.hust.display.entity.Job;
+import com.hust.display.entity.User;
+import com.hust.display.service.JobService;
+import com.hust.display.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("admin")
+public class AdminPageController {
+
+    @Autowired
+    UserService userService;
+    @Autowired //annotation必须要有
+    JobService jobService;
+
+    @RequestMapping("/index")
+    public String indexPage(){
+        return "/admin/index";
+    }
+
+    //不分页
+    @RequestMapping("/manageUser")
+    public String userManagement(Model model){
+        List<User> lists = userService.getAllUser();
+        model.addAttribute("users",lists);
+        return "/admin/manageUser";
+    }
+
+    @RequestMapping("/manageData")
+    public String dataManagement(Model model){
+        List<Job> lists = jobService.getAllJob();
+        model.addAttribute("jobs",lists);
+        return "/admin/manageData";
+    }
+
+
+    @RequestMapping("/login")
+    public String login(){
+        return "/admin/login";
+    }
+
+//    @RequestMapping("/calendar")
+//    public String calendar(){
+//        return "/admin/calendar";
+//    }
+
+}
